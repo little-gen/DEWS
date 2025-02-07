4135a794a16c755e6db89ef2c9509a9830f7d990
Ivan Escudero <ivanesinformatico@gmail.com>
2025-02-07 23:17:20 +0100

    Version Final

--- api.php
+++ api.php
@@ -1,12 +1,31 @@
 <?php
+/**
+ * Comprueba si se recibe un nombre de Pokémon por parámetro y obtiene sus datos de la API.
+ * Muestra la información del Pokémon si se encuentra, o redirecciona en caso contrario.
+ */
 if (isset($_GET['pokemon'])) {
+    /**
+     * @var string $pokemon Nombre del Pokémon recibido por parámetro.
+     */
     $pokemon = $_GET['pokemon'];
+        
+    /**
+     * @var string $url URL de la API de Pokémon para obtener los datos del Pokémon.
+     */
     $url = "https://pokeapi.co/api/v2/pokemon/$pokemon";
+      /**
+     * @var $result Contiene la respuesta de la API en formato JSON o false si falla.
+     */
     $result = file_get_contents($url);
+    //Si el resultado no existe redirecciono a pagina principal y doy un alert
     if ($result === false) {
-        header("Location:index.php");
+         echo "<script>alert('No se encuentra el Pokémon'); "
+        . "window.location.href='index.php';</script>";
         exit();
     }
+       /**
+     * @var object $datos Decodificación de la respuesta JSON de la API.
+     */
     $datos = json_decode($result);
     ?>
 
@@ -60,16 +79,21 @@ if (isset($_GET['pokemon'])) {
     <body>
         <div class="pokemon">
             <?php
+             /**
+             * Verifica si la API devuelve imágenes del Pokémon y las muestra.
+             * Si no existen, muestra una alerta y redirecciona.
+             */
             if(!empty($datos->sprites->front_default)){
             echo '<img src=' . $datos->sprites->front_default . '>';
             echo '<img src=' . $datos->sprites->back_default . '>';
             }else{
-                header("Location:index.php");
-                exit();
+                echo "<script>alert('No se puede cargar la imagen'); "
+                . "window.location.href='index.php';</script>";
+                 exit();
             }
             
             ?>
-            <a href="index.php" class="atras">Buscar</a>
+            <a href="index.php" class="atras">Buscar Otro</a>
         </div>
     </body>
     </html>
